73e1f8d8bfde487a25dde4f419662fc4e6eab722
steve-dev <heyyou248248@gmail.com>
2015-12-09 12:04:24 -0800

    Moved ldap auth plugin fix install to setupRedmine.sh

--- setupRedmine.sh
+++ setupRedmine.sh
@@ -4,3 +4,15 @@ set -e
 REDMINE_SYS_DATA_SQL=redmine-init-system.sql
 
 docker exec pg-redmine gosu postgres psql -d redmine -U redmine -f /${REDMINE_SYS_DATA_SQL}
+
+# copy autoreg plugin
+docker exec redmine git clone https://github.com/steve-dev/redmine_api_auth_fix.git 
+
+docker exec redmine mv redmine_api_auth_fix /home/redmine/redmine/plugins/
+
+# restart redmine - this will break nginx (aka proxy) - must restart nginx (aka proxy)
+docker restart redmine
+
+# Required when redmine is restarted to support the plugin install process.
+docker restart proxy
+
