e2e5b8c3f074e34f44057705022d5f34b9647e76
cgudea <pulala@gmail.com>
2016-01-25 13:32:45 -0800

    fixed source of docker process names issue added error checking ignore backup directory

--- backupRedmine.sh
+++ backupRedmine.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-
 BASEDIR=$(readlink -f $(dirname $0))
+BACKUP_DIR=backups
 
 set -e
 # pull in config variables for container names
-source ${BASEDIR}/config
-source ${BASEDIR}/config.default
+source ${BASEDIR}/../../config
+source ${BASEDIR}/../../config.default
 # pull in common variables for postgres/redmine
-source ./config
+source ${BASEDIR}/config
 
 BACKUP_FN=`echo "$(date +%Y-%m-%d\ %H:%M:%S.%N|cut -c 1-22 | tr ' ' '_').sql"`
 
-echo "Beginning data dump . . . "
-docker exec ${PG_REDMINE_NAME} pg_dumpall -c -U ${PG_USER} -w -f ${BACKUP_FN}_
+echo "Beginning data dump... "
+docker exec ${PG_REDMINE_NAME} pg_dumpall -c -U ${PG_USER} -w -f ${BACKUP_FN} || { 
+    echo "Backup failed!"
+    exit 1
+}
 
-echo "Copying backup . . . "
-mkdir backups
-docker cp ${PG_REDMINE_NAME}:/${BACKUP_FN} ./backups/
+echo "Copying backup... "
+mkdir -p ${BACKUP_DIR} 
+docker cp ${PG_REDMINE_NAME}:/${BACKUP_FN} ${BACKUP_DIR}/ || {
+    echo "Copy failed!"
+    exit 1
+}
+
+echo "Backup Completed!"
