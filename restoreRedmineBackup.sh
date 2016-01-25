d3fba03e10c212f4720aede2c41c8cae8b262d9b
cgudea <pulala@gmail.com>
2016-01-25 15:46:59 -0800

    grab only the file name of backup file

--- restoreRedmineBackup.sh
+++ restoreRedmineBackup.sh
@@ -6,12 +6,14 @@
 #   $1: directory to search
 #
 function get_newest_file(){
-    files=($1/*) newest=${files[0]}
+    cd $1
+    files=(*) newest=${files[0]}
     for f in "${files[@]}"; do
         if [[ $f -nt $newest ]]; then
             newest=$f
         fi
     done
+    cd ..
 }
 
 set -e
@@ -28,11 +30,13 @@ source ${BASEDIR}/../../config.default
 source ${BASEDIR}/config
 
 
-echo "Coping backup to ${PG_REDMINE_NAME} process "
+echo "Coping backup to ${PG_REDMINE_NAME} container "
 docker cp ${BACKUP_DIR}/${BACKUP_FN} ${PG_REDMINE_NAME}:/ || {
     echo "Copy failed!"
     exit 1
 }
+
+echo "Start restore of backup "
 docker exec ${PG_REDMINE_NAME} psql -U ${PG_USER} -f ${BACKUP_FN}  postgres || { 
     echo "Backup failed!"
     exit 1
