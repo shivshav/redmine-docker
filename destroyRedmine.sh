c364cb4b85afefe9bba54704bc6ef202c6523eff
shiv <sprasad0603@gmail.com>
2016-02-04 00:45:16 -0800

    Removing the destroy errors

--- destroyRedmine.sh
+++ destroyRedmine.sh
@@ -4,8 +4,13 @@ PG_REDMINE_NAME=${PG_REDMINE_NAME:-pg-redmine}
 REDMINE_NAME=${REDMINE_NAME:-redmine}
 REDMINE_VOLUME=${REDMINE_VOLUME:-redmine-volume}
 
-docker stop ${REDMINE_NAME}
-docker rm -v ${REDMINE_NAME}
-docker rm -v ${REDMINE_VOLUME}
-docker stop ${PG_REDMINE_NAME}
-docker rm -v ${PG_REDMINE_NAME}
+echo "Removing ${REDMINE_NAME}..."
+docker stop ${REDMINE_NAME} &> /dev/null
+docker rm -v ${REDMINE_NAME} &> /dev/null
+
+echo "Removing ${REDMINE_VOLUME}..."
+docker rm -v ${REDMINE_VOLUME} &> /dev/null
+
+echo "Removing ${PG_REDMINE_NAME}..."
+docker stop ${PG_REDMINE_NAME} &> /dev/null
+docker rm -v ${PG_REDMINE_NAME} &> /dev/null
