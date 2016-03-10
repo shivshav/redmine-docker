47f4dc8e66fc9e0dc1112322a73063d1a37e5939
steve-dev <heyyou248248@gmail.com>
2016-03-09 16:19:08 -0800

    Fixed file copy errors.

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,14 @@ FROM sameersbn/redmine
 MAINTAINER steve-dev@gmail.com
 
 ENV REDMINE_ADMIN_PASSWORD="my_password"
+ENV REDMINE_PLUGIN_DIR=/home/redmine/redmine/plugins
 
 RUN git clone --verbose https://github.com/steve-dev/redmine_api_auth_fix.git /home/redmine/redmine/plugins/redmine_api_auth_fix
 
 COPY entrypoint.custom.sh ${REDMINE_DATA_DIR}/
 COPY redmine-init-system.sql.template ${REDMINE_DATA_DIR}/
+COPY post-install.sh ${REDMINE_PLUGIN_DIR}
 
+RUN chown ${REDMINE_USER}:${REDMINE_GROUP} ${REDMINE_PLUGIN_DIR}/post-install.sh
+RUN chmod 755 ${REDMINE_PLUGIN_DIR}/post-install.sh
 RUN chmod 755 ${REDMINE_DATA_DIR}/entrypoint.custom.sh
--- entrypoint.custom.sh
+++ entrypoint.custom.sh
@@ -4,10 +4,9 @@ set -e
 # post-install.sh vars
 REDMINE_USER=redmine
 REDMINE_GROUP=redmine
-REDMINE_PLUGIN_DIR=/home/redmine/redmine/plugins
 
 # provide name of database file
-REDMINE_SYS_DATA_SQL=redmine-init-system.sql
+REDMINE_SYS_DATA_SQL=redmine-init-system.sql.template
 
 # Send the db to postgres
 PGPASSWORD=$DB_PASS psql --host $DB_HOST --username $DB_USER -d $DB_NAME -f ${REDMINE_DATA_DIR}/${REDMINE_SYS_DATA_SQL}
@@ -15,8 +14,3 @@ PGPASSWORD=$DB_PASS psql --host $DB_HOST --username $DB_USER -d $DB_NAME -f ${RE
 echo ${CI_ADMIN_PWD}
 # Set the admin password
 $REDMINE_INSTALL_DIR/bin/rails runner "u=User.find_by_id(1);u.password=\"${REDMINE_ADMIN_PASSWORD}\";u.save!;"
-
-# Copy post-install script to redmine plugins directory - executes on image start
-${BASEDIR}/post-install.sh ${REDMINE_NAME}:${REDMINE_PLUGIN_DIR}
-${REDMINE_NAME} chown ${REDMINE_USER}:${REDMINE_GROUP} ${REDMINE_PLUGIN_DIR}/post-install.sh
-${REDMINE_NAME} chmod 744 ${REDMINE_PLUGIN_DIR}/post-install.sh
